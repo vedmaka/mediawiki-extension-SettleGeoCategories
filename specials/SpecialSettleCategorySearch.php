16a33514130696191a30801f3b62601d209ad7e3
Vedmaka <god.vedmaka@gmail.com>
2017-01-08 21:36:33 +0300

    + categories search page

--- specials/SpecialSettleCategorySearch.php
+++ specials/SpecialSettleCategorySearch.php
@@ -13,6 +13,8 @@ class SpecialSettleCategorySearch extends SpecialPage {
 
 	public function execute( $subPage ) {
 
+		$this->getOutput()->addModules('ext.settlegeocategories.search');
+
 		$this->templater = new TemplateParser( dirname(__FILE__).'/../templates/', true );
 
 		if( $subPage === null ) {
@@ -29,7 +31,9 @@ class SpecialSettleCategorySearch extends SpecialPage {
 	private function displayCategories( $path ) {
 
 		$data = array(
-			'categories' => array()
+			'categories' => array(),
+			'backlink' => SpecialPage::getTitleFor('SettleCategorySearch')->getFullURL(),
+			'have_results' => false
 		);
 
 		$country = null;
@@ -42,6 +46,7 @@ class SpecialSettleCategorySearch extends SpecialPage {
 		}
 
 		$data['country_text'] = $country;
+		$this->getOutput()->setPageTitle( $country );
 
 	    // Fetch categories for pages within a country
 		$query = SphinxStore::getInstance()->getQuery();
@@ -66,37 +71,43 @@ class SpecialSettleCategorySearch extends SpecialPage {
 			}
 		}
 
-		// Imported from SettleGeoSearch
-		// Fetch
-		$pl1 = ", (";
-		$pl1 .= "( IN( properties.geocodes, {$path[0]} ) ) AND properties.geocategory IS NOT NULL AND IN( properties.geocategoryid, ".implode(',', $topCategoriesIds)." )";
-		$pl1 .= " )";
-		$pl1 .= " AS p";
-		$pl2 = " WHERE p=1";
-		$pl3 = "";
+		if( count($topCategoriesIds) ) {
 
-		$sql = "SELECT *{$pl1} FROM ".SphinxStore::getInstance()->getIndex()."{$pl3}{$pl2} LIMIT 0,10000 OPTION ranker=matchany;";
+			$data['have_results'] = true;
 
-		$result = $query->query( $sql )->execute();
+			// Imported from SettleGeoSearch
+			// Fetch
+			$pl1 = ", (";
+			$pl1 .= "( IN( properties.geocodes, {$path[0]} ) ) AND properties.geocategory IS NOT NULL AND IN( properties.geocategoryid, " . implode( ',', $topCategoriesIds ) . " )";
+			$pl1 .= " )";
+			$pl1 .= " AS p";
+			$pl2 = " WHERE p=1";
+			$pl3 = "";
 
-		$categories = array();
+			$sql = "SELECT *{$pl1} FROM " . SphinxStore::getInstance()->getIndex() . "{$pl3}{$pl2} LIMIT 0,10000 OPTION ranker=matchany;";
 
-		if( $result->count() ) {
-			foreach ( $result as $r ) {
+			$result = $query->query( $sql )->execute();
 
-				$properties = json_decode( $r['properties'], true );
-				$categories[ $properties['geocategoryid'][0] ] = $properties['geocategory'][0];
+			$categories = array();
+
+			if ( $result->count() ) {
+				foreach ( $result as $r ) {
+
+					$properties                                    = json_decode( $r['properties'], true );
+					$categories[ $properties['geocategoryid'][0] ] = $properties['geocategory'][0];
+				}
 			}
+
+			$categories_html = '';
+
+			// Prepare for mustache
+			foreach ( $categories as $id => $category ) {
+				$categories_html .= $this->displayCategoryRecursive( new SettleGeoCategory( $id ) );
+			}
+
+			$data['categories_html'] = $categories_html;
+
 		}
-
-		$categories_html = '';
-
-		// Prepare for mustache
-		foreach ($categories as $id => $category) {
-			$categories_html .= $this->displayCategoryRecursive( new SettleGeoCategory( $id ) );
-		}
-
-		$data['categories_html'] = '<ul>' . $categories_html . '</ul>';
 
 		$html = $this->templater->processTemplate( 'search_categories', $data );
 		$this->getOutput()->addHTML( $html );
@@ -117,9 +128,19 @@ class SpecialSettleCategorySearch extends SpecialPage {
             'url_prefix' => SpecialPage::getTitleFor('SettleCategorySearch')->getFullURL()
         );
 
+		$query = SphinxStore::getInstance()->getQuery();
         $sortedByChar = array();
 
         foreach ($countries as $country) {
+
+	        $result = $query->query('SELECT id, IN( properties.country_code, '.$country['geonamesCode'].' ) AS p FROM wiki_rt WHERE p = 1')->execute();
+	        //if( !$result->count() ) {
+		        // Do not include countries without articles
+		    //    continue;
+	        //}
+
+	        $country['articles_count'] = $result->count() ? $result->count() : false;
+
             $name = $country['name'];
             $firstChar = mb_strtoupper( mb_substr( $name, 0, 1 ) );
             //TODO: fix unicode characters
@@ -132,10 +153,11 @@ class SpecialSettleCategorySearch extends SpecialPage {
                 );
             }
             $country['url_prefix'] = $data['url_prefix'];
+
             $sortedByChar[ $firstChar ]['items'][] = $country;
         }
 
-        //TODO:
+        //TODO: ?
         array_pop( $sortedByChar );
 
         foreach ($sortedByChar as $s) {
@@ -162,7 +184,8 @@ class SpecialSettleCategorySearch extends SpecialPage {
 			'articles' => array(),
 			'title' => $category->getTitleKey(),
 			'url' => SpecialPage::getTitleFor('Category')->getFullURL().'/'.$category->getId(),
-			'innerHtml' => ''
+			'innerHtml' => '',
+			'articles_count' => false
 		);
 
 		$articles = SettleGeoCategories::getPagesInCategory( $category->getId() );
@@ -173,6 +196,7 @@ class SpecialSettleCategorySearch extends SpecialPage {
 					'url' => $article->getFullURL()
 				);
 			}
+			//$data['articles_count'] = count($articles);
 		}
 
 		if( $category->getChildren() ) {
